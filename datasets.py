f87d6c18d0d7e3a917bd19ae8a747ebff9d44d0a
Shimon Malnick <shimonmalnick@gmail.com>
2024-03-16 17:01:23 +0200

    Added datasets for MSCOCO, Chest Xray and Random images, with support for running experiment on these images

---
+++ datasets.py
@@ -0,0 +1,120 @@
+import json
+import os
+import torch
+from matplotlib import pyplot as plt
+from p2p.p2p_functions import load_im_into_format_from_path
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.transforms import ToPILImage, ToTensor
+
+COCO_ROOT = "/home/shimon/research/datasets/mscoco17"
+CHEST_XRAY_ROOT = "/home/shimon/research/datasets/chest_xray"
+
+
+class CocoCaptions17(Dataset):
+    def __init__(self, root=COCO_ROOT, transform=None):
+        self.images_root = f"{root}/val2017"
+        self.images_list = os.listdir(self.images_root)
+        self.len = len(self.images_list)
+        with open(f"{root}/annotations/captions_val2017.json") as f:
+            self.captions_map = self._parse_captions(json.load(f)['annotations'])
+        if transform is None:
+            self.transform = transforms.Compose([load_im_into_format_from_path])
+        else:
+            self.transform = transform
+
+    @staticmethod
+    def _parse_captions(captions):
+        captions_map = {}
+        for d in captions:
+            img_id = d['image_id']
+            if img_id not in captions_map:
+                captions_map[img_id] = d['caption']
+        return captions_map
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, item):
+        return self._get_single_item(item)
+
+    def _get_single_item(self, item):
+        image_path = self.get_image_path_by_index(item)
+        image = self.transform(image_path)
+        image_id = int(self.images_list[item].split(".")[0])
+        caption = self.captions_map[image_id]
+        return image, caption
+
+    def get_image_path_by_index(self, index):
+        return os.path.join(self.images_root, self.images_list[index])
+
+
+class ChestXRay(Dataset):
+    def __init__(self, root=CHEST_XRAY_ROOT, transform=None):
+        self.image_dirs = ["images_0" + ("0" + str(i) if i < 10 else str(i)) for i in range(1, 12)]
+        self.data_root = root
+        self.images = self.__create_images_data()
+        self.static_caption = "A chest X-ray image"
+        if transform is None:
+            self.transform = transforms.Compose([load_im_into_format_from_path])
+        else:
+            self.transform = transform
+
+    def __create_images_data(self):
+        images_list = []
+        for image_dir in self.image_dirs:
+            cur_images_root = os.path.join(self.data_root, image_dir, "specific_images")
+            cur_images = sorted(os.listdir(cur_images_root))
+            images_list.extend([os.path.join(cur_images_root, im) for im in cur_images])
+        return images_list
+
+    def __getitem__(self, item):
+        return self.transform(self.images[item]), self.static_caption
+
+    def __len__(self):
+        return len(self.images)
+
+
+class NormalDistributedDataset(Dataset):
+    def __init__(self, ds_size=500, images_shape=(3, 512, 512)):
+        self.ds_size = ds_size
+        self.data = torch.randn(ds_size, *images_shape)
+        self.static_caption = "A random noise image"
+        self.transform = ToPILImage()
+
+    def __len__(self):
+        return self.ds_size
+
+    def __getitem__(self, item):
+        return self.transform(self.data[item]), self.static_caption
+
+
+class FolderDataset(Dataset):
+    def __init__(self, root, static_caption, transform=None):
+        self.root = root
+        self.images = os.listdir(root)
+        self.len = len(self.images)
+        self.static_catpion = static_caption
+        if transform is None:
+            self.transform = transforms.Compose([load_im_into_format_from_path])
+        else:
+            self.transform = transform
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, item):
+        return self.transform(os.path.join(self.root, self.images[item])), self.static_catpion
+
+
+if __name__ == '__main__':
+    import PIL.Image as Image
+    ds = CocoCaptions17(transform=lambda x: Image.open(x))
+    # ds = ChestXRay(transform=lambda x: Image.open(x))
+    # ds = NormalDistributedDataset()
+    for i in range(5):
+        print("image path:", ds.get_image_path_by_index(i))
+        cur_image, cur_caption = ds[i]
+        print(cur_caption)
+        plt.imshow(cur_image)
+        plt.show()
--- main_likelihoods.py
+++ main_likelihoods.py
@@ -1,55 +1,50 @@
 import os
+from glob import glob
+from typing import List
+import numpy as np
 import torch
 from diffusers import DDIMScheduler, StableDiffusionImg2ImgPipeline
+from matplotlib import pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from inversion import MyInvertFixedPoint
-from p2p.p2p_functions import load_im_into_format_from_path
 from torch.distributions import Normal
-import torchvision.transforms as transforms
-from torch.utils.data.dataset import Dataset
-import json
 import logging
+from datasets import CocoCaptions17, ChestXRay, NormalDistributedDataset, FolderDataset
+from dataclasses import dataclass
+import shutil
 
-COCO_ROOT = "/home/shimon/research/datasets/mscoco17"
+
+def run_coco(save_dir=None, num_ddim_step=50):
+    coco_ds = CocoCaptions17()
+    if save_dir is None:
+        save_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", "coco_inverse_latents")
+    run(coco_ds, save_dir, num_ddim_step=num_ddim_step)
 
 
-class CocoCaptions17(Dataset):
-    def __init__(self, root=COCO_ROOT, transform=None):
-        self.images_root = f"{root}/val2017"
-        self.images_list = os.listdir(self.images_root)
-        self.len = len(self.images_list)
-        with open(f"{root}/annotations/captions_val2017.json") as f:
-            self.captions_map = self._parse_captions(json.load(f)['annotations'])
-        if transform is None:
-            self.transform = transforms.Compose([load_im_into_format_from_path])
-        else:
-            self.transform = transform
+def run_chest_x_ray(save_dir=None, num_ddim_step=50):
+    chest_x_ray_dataset = ChestXRay()
+    if save_dir is None:
+        save_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", "chest_x_rays")
+    run(chest_x_ray_dataset, save_dir, dataset_indices=(0, 50), num_ddim_step=num_ddim_step)
 
-    def _parse_captions(self, captions):
-        captions_map = {}
-        for d in captions:
-            img_id = d['image_id']
-            if img_id not in captions_map:
-                captions_map[img_id] = d['caption']
-        return captions_map
 
-    def __len__(self):
-        return self.len
+def run_normal_distributed_images(save_dir=None, num_ddim_step=50):
+    normal_dataset = NormalDistributedDataset()
+    if save_dir is None:
+        save_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", "random_normal")
+    run(normal_dataset, save_dir, save_images=True, dataset_indices=(0, 50), num_ddim_step=num_ddim_step)
 
-    def __getitem__(self, item):
-        return self._get_single_item(item)
 
-    def _get_single_item(self, item):
-        image_path = os.path.join(self.images_root, self.images_list[item])
-        image = self.transform(image_path)
-        image_id = int(self.images_list[item].split(".")[0])
-        caption = self.captions_map[image_id]
-        return image, caption
+def examine_specific_images(images_dir, caption, save_dir):
+    dataset = FolderDataset(images_dir, caption)
+    save_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", save_dir)
+    run(dataset, save_dir, save_images=True)
 
 
 @torch.no_grad()
-def run():
-    coco_ds = CocoCaptions17()
-    save_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", "coco_inverse_latents")
+def run(dataset, save_dir, save_images=False, dataset_indices=None, num_ddim_step=50):
+    if dataset_indices is None:
+        dataset_indices = (0, len(dataset))
     logging.info(f"Saving results to {save_dir}")
     os.makedirs(save_dir, exist_ok=True)
     torch.manual_seed(8888)
@@ -67,12 +62,10 @@ def run():
                                                            ).to("cuda")
 
     GUIDANCE_SCALE = 2
-    NUM_DDIM_STEPS = 50
     standard_normal = Normal(0, 1)
 
-    # for i in range(len(coco_ds)):
-    for i in range(33, len(coco_ds)):
-        image, caption = coco_ds[i]
+    for i in range(*dataset_indices):
+        image, caption = dataset[i]
         if len(image.getbands()) < 3:
             logging.info(f"Skipping image {i} as it is not rgb")
             continue
@@ -80,7 +73,7 @@ def run():
                             output_type="latent").images
 
         # RUN FP inversion on vae_latent
-        latent = pipe.invert(caption, latents=new_latents, num_inference_steps=NUM_DDIM_STEPS,
+        latent = pipe.invert(caption, latents=new_latents, num_inference_steps=num_ddim_step,
                              guidance_scale=GUIDANCE_SCALE, num_iter=20).latents
 
         # we examine the latents log-likelihood compared to standard normal distribution R.V,
@@ -90,13 +83,72 @@ def run():
         output = {"log_likelihood": log_likelihood.item(),
                   "latent": latent,
                   "prompt": caption,
-                  "NUM_DDIM_STEPS": NUM_DDIM_STEPS,
+                  "NUM_DDIM_STEPS": num_ddim_step,
                   "GUIDANCE_SCALE": GUIDANCE_SCALE,
                   }
-        logging.info(f"finished image {i + 1}/{len(coco_ds)}")
-        torch.save(output, os.path.join(save_dir, f"coco_{i}.pt"))
+        if save_images:
+            output['image'] = image
+        logging.info(f"finished image {i + 1}/{len(dataset)}")
+        torch.save(output, os.path.join(save_dir, f"sample_{i}.pt"))
+
+
+def fix_filenames(basedir):
+    names = os.listdir(basedir)
+    for name in names:
+        if name.startswith("coco_"):
+            new_name = name.replace("coco", "sample")
+            shutil.move(os.path.join(basedir, name), os.path.join(basedir, new_name))
+
+
+@dataclass
+class HistogramData:
+    directory: str
+    name: str
+
+
+def plot_likelihoods_histograms(data_arr: List[HistogramData], n_samples_per_data=50, save_path=None):
+    plot_data = {}
+    for hist_data in data_arr:
+        files = os.listdir(hist_data.directory)
+        likelihoods = [torch.load(os.path.join(hist_data.directory, f))["log_likelihood"] for f in files][
+                      :n_samples_per_data]
+        assert len(likelihoods) == n_samples_per_data,\
+            f"Required n_samples_per_data samples ( = {n_samples_per_data}, but got only {len(likelihoods)}"
+        plot_data[hist_data.name] = np.asarray(likelihoods)
+    plt.hist(list(plot_data.values()), label=list(plot_data.keys()))
+    plt.legend()
+    plt.xlabel("NLL")
+    plt.ylabel("Count")
+    plt.title(f"Histogram (over {n_samples_per_data}) NLL of latents inversed from different images")
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
+
+
+def plot_specific_images_likelihoods(results_dir):
+    samples_dicts = [torch.load(os.path.join(results_dir, f)) for f in os.listdir(results_dir)]
+    likelihoods = [d["log_likelihood"] for d in samples_dicts]
+    images = [d["image"] for d in samples_dicts]
+    os.makedirs(os.path.join(results_dir, "images"), exist_ok=True)
+    for idx, img in enumerate(images):
+        plt.figure()
+        plt.imshow(img)
+        plt.title(f"NLL = {likelihoods[idx]}")
+        plt.savefig(os.path.join(results_dir, "images", f"sample_{idx}.png"))
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    run()
+    base_dir = "/home/shimon/research/diffusion_inversions/FPI/results"
+    for ddim_step in [1] + list(range(5, 51, 5)):
+        cur_base_dir = f"{base_dir}/num_ddim_steps_{ddim_step}"
+        run_coco(save_dir=f"{cur_base_dir}/coco", num_ddim_step=ddim_step)
+        run_chest_x_ray(save_dir=f"{cur_base_dir}/chest_x_ray", num_ddim_step=ddim_step)
+        run_normal_distributed_images(save_dir=f"{cur_base_dir}/random_normal", num_ddim_step=ddim_step)
+
+    # coco_data_hist = HistogramData(directory=f"{base_dir}/coco_inverse_latents", name="MS-COCO")
+    # chest_data_hist = HistogramData(directory=f"{base_dir}/chest_x_rays", name="ChestXRay")
+    # random_data_hist = HistogramData(directory=f"{base_dir}/random_normal", name="Random noise")
+    # data_hists = [coco_data_hist, chest_data_hist, random_data_hist]
+    # plot_likelihoods_histograms(data_hists, save_path=f"{base_dir}/likelihoods_histograms.png")
+
