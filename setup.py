351d91e2608030fbdcc278fb1e24516b46050135
Shimon Malnick <shimonmalnick@gmail.com>
2024-06-02 19:58:10 +0300

    added a variable for output directory root

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ import os
 setup_config = {
 
     "DATASETS_ROOT": "/home/shimon/research/datasets",
-    "REPO_BASE": "/home/shimon/research/diffusion_inversions/FPI"
+    "REPO_BASE": "/home/shimon/research/diffusion_inversions/FPI",
+    "OUTPUT_ROOT": "/home/shimon/research/diffusion_inversions/results",
 }
 
 setup_config = {k: os.environ[k] if k in os.environ else v for k, v in setup_config.items()}
