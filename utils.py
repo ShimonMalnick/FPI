9f1e304fe3bfc83703b021c0fcc39aee41f55de8
Shimon Malnick <shimonmalnick@gmail.com>
2024-06-05 12:08:53 +0300

    Added func to plot bpd histogram

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
-from typing import Union
+import math
+from typing import Union, Dict
 import torch
 import numpy as np
 from diffusers import DDIMScheduler
+from matplotlib import pyplot as plt
 from torch.distributions import Normal
 from inversion import MyInvertFixedPoint
 
@@ -43,3 +45,28 @@ def latent_to_image(latent, num_ddim_steps=10, pipe=None, save_path=None):
     if save_path:
         image.save(save_path)
     return image
+
+
+def plot_bpd_histogram(data: Dict, save_path=None, show_fig=True, n_bins=None):
+    """
+    Plot the BPD of the given data. the data is expected as a dictionary of the form {label: array of bpd values}
+    :param data: the data to plot
+    :param save_path: the path to save the plot, leave None to not save
+    :param show_fig: whether to show the figure
+    :param n_bins: the number of bins to use in the histogram. if left blank, the maximal square root of the number of
+    values in each data sample
+    """
+    plt.clf()
+    plt.figure(figsize=(10, 10))
+    if n_bins is None:
+        n_bins = max([math.sqrt(len(v)) for v in data.values()])
+    for label, values in data.values():
+        plt.hist(values, label=label, bins=n_bins)
+    plt.legend(loc='upper right')
+    plt.xlabel("BPD")
+    plt.ylabel("Count")
+    if save_path:
+        plt.savefig(save_path)
+    if show_fig:
+        plt.show()
+    plt.close()
