fcefce9d81b4d9e185ae16353bd26d32b96d15e8
Shimon Malnick <shimonmalnick@gmail.com>
2024-06-14 12:21:43 +0300

    changed the entry script to support null space runs

--- entry_script.sh
+++ entry_script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd /storage/malnick/FPI/ && conda init bash
+cd /storage/malnick/FPI/null_space && conda init bash
 echo "source activate fpi" > ~/.bashrc && source ~/.bashrc
-nohup python examine_likelihoods_partial_inverse.py > ../logs/out_coco.log 2> ../logs/err_coco.log
+wandb login 7a46bfc8dff80b42bd3001321c9b895b663cc318
+nohup python train.py --attention_trainable value --output_dir results_null_space_values > ../../logs/out_values.log 2> ../../logs/err_values.log
+#python train.py --attention_trainable query --output_dir results_null_space_queries
